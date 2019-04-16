5d0ab4de2b12b717a33677a8d1a2ae6a427a0f03
Anthony Williams <anthony.williams.cs@gmail.com>
2019-04-15 21:59:03 -0600

    Start color tests

--- ConHelper/Program.cs
+++ ConHelper/Program.cs
@@ -6,7 +6,14 @@ namespace ConHelper
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Before 1...");
+			Console.WriteLine("Before 2...");
+			Console.BackgroundColor = ConsoleColor.DarkCyan;
+			Console.ForegroundColor = ConsoleColor.White;
 			Console.WriteLine("Hello World!");
+			Console.ResetColor();
+			Console.WriteLine("After 1...");
+			Console.WriteLine("After 2...");
 		}
 	}
 }
