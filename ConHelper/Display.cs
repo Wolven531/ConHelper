8871ad4f5101c5f3e98ae5ebf2bb3bd9832c6ad2
Anthony Williams <anthony.williams.cs@gmail.com>
2019-04-16 20:50:14 -0600

    Separate Display class

---
+++ ConHelper/Display.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConHelper
+{
+	public class Display
+	{
+		public Display()
+		{
+		}
+
+		public static void ShowOptions(IEnumerable<string> options)
+		{
+			var optionDisplay = 1;
+			var originalForeground = Console.ForegroundColor;
+			foreach (var option in options)
+			{
+				Console.ForegroundColor = ConsoleColor.Cyan;
+				Console.Write($"{optionDisplay}.) ");
+				Console.ForegroundColor = originalForeground;
+				Console.WriteLine(option);
+				optionDisplay++;
+			}
+		}
+	}
+}
--- ConHelper/Program.cs
+++ ConHelper/Program.cs
@@ -1,26 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace ConHelper
 {
-	class Display
-	{
-		public static void ShowOptions(IEnumerable<string> options)
-		{
-			var optionDisplay = 1;
-			var originalForeground = Console.ForegroundColor;
-			foreach (var option in options)
-			{
-				Console.ForegroundColor = ConsoleColor.Cyan;
-				Console.Write($"{optionDisplay}.) ");
-				Console.ForegroundColor = originalForeground;
-				Console.WriteLine(option);
-				optionDisplay++;
-			}
-		}
-	}
-	
 	class Program
 	{
 		static void Main(string[] args)
